2cbb30cb1ed9cb839bf4f5608d5706d6e2e59126
handikadicky <handikadicky@gmail.com>
2023-09-01 10:02:10 +0700

    add redirect to figma prototype

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
  <template>
     <div>
-        <p>hello world</p>
+        <p></p>
     </div>
 </template>
+
+<script setup>
+
+window.location.replace(
+        "https://www.figma.com/file/dSf6NWpUKPp6cIhdAbwnZa/dickyudhandika.com?type=design&node-id=0%3A1&mode=design&t=1S3RxniiRlTnPFkh-1"
+      );
+
+</script>
